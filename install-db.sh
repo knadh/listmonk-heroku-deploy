ba36d0c79b11d0bb32fea4a34062446110948e24
Kailash Nadh <kailash@nadh.in>
2021-08-21 17:43:13 +0530

    Add psql in setup to update URLs in setting on install

--- install-db.sh
+++ install-db.sh
@@ -5,3 +5,8 @@ source ./env.sh
 
 # Setup the listmonk DB.
 ./listmonk --install --yes
+
+# Update the app's root URL.
+psql $DATABASE_URL -c \
+	"UPDATE settings SET value='\"$ROOT_URL\"' WHERE key='app.root_url'; \
+	 UPDATE settings SET value='\"$ROOT_URL/public/static/logo.png\"' WHERE key='app.logo_url';"
